29ede2ba06a3bd3a70309468d1e11315dc51a90f
Guillaume Hanotel <guillaumehanotel@orange.fr>
2021-05-07 11:34:40 +0200

    install linter

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-import Joi from "joi";
+import Joi from 'joi';
 
-const form = document.querySelector(".main-form");
+const form = document.querySelector('.main-form');
 
 const validateUser = (data) => {
   const schema = Joi.object({
     firstname: Joi.string().alphanum().min(4).max(20).required(),
     lastname: Joi.string().alphanum().min(2).max(10).required(),
-  }).with("firstname", "lastname");
+  }).with('firstname', 'lastname');
   return schema.validate(data);
 };
 
-form.addEventListener("submit", function (event) {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const formData = new FormData(form);
 
-  let data = {
-    firstname: formData.get("firstname"),
-    lastname: formData.get("lastname"),
+  const data = {
+    firstname: formData.get('firstname'),
+    lastname: formData.get('lastname'),
   };
 
-  let isValidated = validateUser(data);
-  console.log(isValidated);
+  const {value, error} = validateUser(data);
+
+  if(error) {
+    console.error(error);
+  }
+
+  console.log(value);
 });
